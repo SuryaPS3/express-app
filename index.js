d3f9cbf592ec129240659693d377580cff31e0e1
SuryaPS3 <spsingh2003.sps@gmail.com>
2025-11-13 12:32:18 +0530

    Added a dynamic route to the url, also understood how app.Method(path,handler) is the general method in creating routes in express

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/',(req,res)=>{
     res.send("Hello Express");
 })
 
+//dynamic route 
+app.get('/user/:username',(req,res)=>{
+    const uname = req.params.username;
+    res.send(`Hello, ${uname}`);
+});
 //to start the server
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
