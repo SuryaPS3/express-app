a5a251bdec8e23d3a6567dd3a617261356659b79
SuryaPS3 <spsingh2003.sps@gmail.com>
2025-11-14 06:04:50 +0530

    Post method but first we have to parse using express.json()(middleware)

---
+++ controller.js
@@ -0,0 +1,7 @@
+export const userLogin = (req,res)=>{
+    res.send("User login");
+}
+
+export const userSignup = (req,res)=>{
+    res.send("User signup")
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+import { userLogin, userSignup } from './controller.js';
+import express from 'express';
 
 const app = express();
 
@@ -9,11 +10,20 @@ app.get('/',(req,res)=>{
     res.send("Hello Express");
 })
 
+app.get('/user/login',userLogin);
+app.get('/user/signup',userSignup);
 //dynamic route 
 app.get('/user/:username',(req,res)=>{
     const uname = req.params.username;
     res.send(`Hello, ${uname}`);
 });
+
+//adding post method
+app.post('/user/create',express.json(),(req,res)=>{
+    const {name,email} =  req.body;
+    res.json({message:`User ${name} with email ${email} created successfully`});
+})
+
 //to start the server
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
